5517cd714d5038746fb141d3ca0bc47e1fc1f6f4
phplaber <phplaber@gmail.com>
2012-08-30 09:15:08 +0800

    创建后台入口文件，访问后台文件自动生成后台目录

--- admin.php
+++ admin.php
@@ -6,8 +6,8 @@ define('THINK_PATH', 'ThinkPHP');
 
 //定义项目名称和路径
 define('ROOT_PATH', '.');
-define('APP_NAME', 'cms');
-define('APP_PATH', './cms');
+define('APP_NAME', 'admin');
+define('APP_PATH', './admin');
 
 //自定义核心编译缓存文件和项目编译缓存文件路径
 define('RUNTIME_PATH', APP_PATH.'/Run/');
