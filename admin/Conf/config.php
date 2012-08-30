fdbfbd065c7c98bda903f2ce639e340750cc8c49
phplaber <phplaber@gmail.com>
2012-08-31 00:28:42 +0800

    change the config file of background

--- admin/Conf/config.php
+++ admin/Conf/config.php
@@ -1,5 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'APP_DEBUG' => true, // 关闭调试模式
+	'DB_TYPE'=> 'mysql',      // 数据库类型
+	'DB_HOST'=> 'localhost', // 数据库服务器地址
+	'DB_NAME'=>'efucms',  // 数据库名称
+	'DB_USER'=>'root', // 数据库用户名
+	'DB_PWD'=>'198802102357', // 数据库密码
+	'DB_PORT'=>'3306', // 数据库端口
+	'DB_PREFIX'=>'efu_', // 数据表前缀
+
+
+	'APP_COMPANY'=>'开源软件实验室',
 );
 ?>
