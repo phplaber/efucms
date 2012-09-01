46323e5c34343d64d71f870ed5163c961f5f8657
phplaber <phplaber@gmail.com>
2012-09-01 23:42:55 +0800

    micro change

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 配置文件 */
+return array(
+	// 数据库配置
+	'APP_DEBUG' => false, // 关闭调试模式
+	'DB_TYPE'=> 'mysql',      // 数据库类型
+	'DB_HOST'=> 'localhost', // 数据库服务器地址
+	'DB_NAME'=>'efucms',  // 数据库名称
+	'DB_USER'=>'root', // 数据库用户名
+	'DB_PWD'=>'198802102357', // 数据库密码
+	'DB_PORT'=>'3306', // 数据库端口
+	'DB_PREFIX'=>'efu_', // 数据表前缀
+
+	// 应用配置
+	'APP_NAME'=>'Easy For Use CMS',
+	'APP_METAKEY'=>'CMS, 简单易用',
+	'APP_METADESC'=>'EFUcms是一款简单易用的内容管理系统，提供了CMS所有最基本的功能。',
+	'APP_YEAR'=>'2012',
+	'APP_COMPANY'=>'开源软件实验室',
+	'COMPANY_URL'=>'http://www.phplabor.com',
+	'APP_ICP'=>'苏ICP备11033855号-1',
+	'LIST_PAGE' =>'5',	// 栏目列表每页展示文章数
+);
+?>
