66c2dcba7ffcbd98491b2481a32fa0924366c54e
phplaber <phplaber@gmail.com>
2012-09-01 23:23:17 +0800

    change ignore file

--- admin/Lib/Action/IndexAction.class.php
+++ admin/Lib/Action/IndexAction.class.php
@@ -1,28 +1,54 @@
 <?php
-// 本文档自动生成，仅供测试运行
+
 class IndexAction extends Action
 {
-    /**
-    +----------------------------------------------------------
-    * 默认操作
-    +----------------------------------------------------------
-    */
+	// 显示登陆表单
     public function index()
     {
-        $this->display(THINK_PATH.'/Tpl/Autoindex/hello.html');
+		$this->display('index/login');
     }
+	
+	// 处理登陆表单提交数据
+	public function login()
+	{
+		$obj_admin = M('Admin');
+		$admin = $obj_admin->find(1);
+		//print_r($admin);
+		if ($_POST['username'] == $admin['username'] && md5($_POST['password']) == $admin['password'])
+		{
+			session_start();
+			$_SESSION['uid'] = $admin['id'];
+			$_SESSION['username'] = $admin['username'];
+			$_SESSION['password'] = $admin['password'];
+			$this->redirect('index/home');
+		}
+		else
+		{
+			exit('没有权限');
+		}
+	}
+	
+	// 显示后台首页
+	public function home()
+	{
+		if (isset($_SESSION['uid']))
+			$this->display('index/home');
+		else
+			$this->redirect('index/index');	// 重定向到index模块的index操作
+	}
 
-    /**
-    +----------------------------------------------------------
-    * 探针模式
-    +----------------------------------------------------------
-    */
-    public function checkEnv()
-    {
-        load('pointer',THINK_PATH.'/Tpl/Autoindex');//载入探针函数
-        $env_table = check_env();//根据当前函数获取当前环境
-        echo $env_table;
-    }
+	// 登出后台管理
+	public function logout()
+	{
+		if (isset($_SESSION['uid']))
+		{
+			unset($_SESSION['uid']);
+			unset($_SESSION['username']);
+			unset($_SESSION['password']);
+		}
+		$this->display('index/login');
+	}
+
 
 }
 ?>
