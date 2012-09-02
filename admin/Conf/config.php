c49923c1c869d17de45dad94c4807b83f9f6545d
phplaber <phplaber@gmail.com>
2012-09-02 18:09:31 +0800

    创建“全局配置”模块

--- admin/Conf/config.php
+++ admin/Conf/config.php
@@ -1,16 +1,17 @@
 <?php
-return array(
-	//'配置项'=>'配置值'
-	'APP_DEBUG' => true, // 关闭调试模式
-	'DB_TYPE'=> 'mysql',      // 数据库类型
-	'DB_HOST'=> 'localhost', // 数据库服务器地址
-	'DB_NAME'=>'efucms',  // 数据库名称
-	'DB_USER'=>'root', // 数据库用户名
-	'DB_PWD'=>'198802102357', // 数据库密码
-	'DB_PORT'=>'3306', // 数据库端口
-	'DB_PREFIX'=>'efu_', // 数据表前缀
 
+if (!defined('THINK_PATH')) exit();
+$config = require "config.php";
 
-	'APP_COMPANY'=>'开源软件实验室',
+$config_list = array(
+	'URL_CASE_INSENSITIVE'  => true,	// URL不区分大小写（对Linux有效）
+	'TOKEN_ON'=>true,	// 是否开启令牌验证
+	'TOKEN_NAME'=>'__hash__',	// 令牌验证的表单隐藏字段名称
+	'TOKEN_TYPE'=>'md5',	//令牌哈希验证规则 默认为MD5
+	'TMPL_CACHE_TIME'		=>	0,	// 关闭模板缓存
 );
+
+//合并输出配置
+return array_merge($config, $config_list);
+
 ?>
--- admin/Lib/Action/ConfigAction.class.php
+++ admin/Lib/Action/ConfigAction.class.php
@@ -1,7 +1,18 @@
 <?php
-
+/* 网站配置模块 */
 class ConfigAction extends Action
 {
-
+	public function index()
+	{
+		if (!isset($_SESSION['uid']))
+			$this->redirect('index/index');	// 重定向到index模块的index操作
+		else
+		{
+			$config = require "config.php";
+			//print_r($config);
+			$this->assign("config", $config);
+			$this->display();
+		}
+	}
 }
 ?>
