7d0ea2426e3804b6fa16b6f8c44e96b3f3a40778
phplaber <phplaber@gmail.com>
2012-09-05 22:51:36 +0800

    创建“栏目管理”模块

--- admin/Lib/Action/ConfigAction.class.php
+++ admin/Lib/Action/ConfigAction.class.php
@@ -9,8 +9,14 @@ class ConfigAction extends Action
 		else
 		{
 			$config = require "config.php";
-			//print_r($config);
 			$this->assign("config", $config);
+
+			// 如果保存成功，返回信息
+			if(isset($_SESSION['message_config']))
+			{
+				$this->assign("message_config", $_SESSION['message_config']);
+				unset($_SESSION['message_config']);
+			}
 			$this->display();
 		}
 	}
@@ -24,16 +30,27 @@ class ConfigAction extends Action
 			$config = require "config.php";
 
 			//print_r($config);
+			$str_config = "<?php return array(";
+			array_push($_POST, $_POST['__hash__']=0);
+			array_pop($_POST);
+			$_POST = array_filter($_POST);
 			foreach($_POST as $key=>$value)
 			{
 				$key = strtoupper($key);
+				//$value = addslashes($value);
 				if(trim($value) != $config[$key])
 					$config[$key] = trim($value);
+				$str_config .= "'".$key."'=>"."'".$config[$key]."',";
 			}
-			array_pop($config);
-			$str_config = substr(file_get_contents('config.php'), 5);
-			echo $str_config;
-			
+			$str_config = substr($str_config, 0, -1)."); ?>";
+			if(file_put_contents('config.php', $str_config))
+			{
+				session_start();
+				$_SESSION['message_config'] = "全局设置已更新 :)";
+				$this->redirect('config/index');
+			}
+			else
+				$this->error('更新失败！');
 		}
 	}
 }
--- admin/Lib/Action/MenuAction.class.php
+++ admin/Lib/Action/MenuAction.class.php
@@ -2,6 +2,19 @@
 
 class MenuAction extends Action
 {
-
+	public function index()
+	{
+		$obj_menu = M('Menu');
+		$obj_content = M('Content');
+		$menuitem = $obj_menu->field('id, title, status, ordering')->select();
+		for($i=0; $i<count($menuitem); $i++)
+		{
+			$menuitem[$i]['articlesum'] = $obj_content->where('menuid='.$menuitem[$i]['id'])->count();
+			array_push($menuitem[$i]['articlesum']);
+		}
+		//print_r($menuitem);
+		$this->assign("menuitem", $menuitem);
+		$this->display();
+	}
 }
 ?>
