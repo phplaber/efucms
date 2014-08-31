094dd6d92d5b321b05f273ee711b70bfc39e3656
yanan <songyanan@tuniu.com>
2014-08-31 15:39:02 +0800

    Add captcha code supported by securimage class

--- admin/Lib/Action/IndexAction.class.php
+++ admin/Lib/Action/IndexAction.class.php
@@ -5,25 +5,43 @@ class IndexAction extends Action
 	// 显示登陆表单
     public function index()
     {
+		// 导入验证码生成类
+		vendor("Securimage.securimage");
+		$img = new Securimage();
+		$options = array(
+				'show_audio_button' => false,
+				'show_text_input' => false,
+				'input_text' => '验证码'
+		);
+		// 生成验证码并传给view
+		$this->assign('code', $img->getCaptchaHtml($options));
+
 		$this->display('index/login');
     }
-	
+
 	// 处理登陆表单提交数据
 	public function login()
 	{
 		$obj_admin = M('Admin');
 		$admin = $obj_admin->find(1);
+		vendor("Securimage.securimage");
+		$img = new Securimage();
 		//print_r($admin);
-		if ($_POST['username'] == $admin['username'] && md5($_POST['password']) == $admin['password'])
+		if ($_POST['username'] == $admin['username'] && md5($_POST['password']) == $admin['password'] && $img->check($_POST['captcha_code']) == true)
 		{
 			session_start();
 			$_SESSION['uid'] = $admin['id'];
 			$_SESSION['username'] = $admin['username'];
 			$_SESSION['password'] = $admin['password'];
 			$this->redirect('index/home');
+		} elseif ($img->check($_POST['captcha_code']) == false) 
+		{
+			header('Content-Type: text/html;charset=utf-8');
+			exit('验证码不正确');
 		}
 		else
 		{
+			header('Content-Type: text/html;charset=utf-8');
 			exit('没有权限');
 		}
 	}
@@ -46,6 +64,6 @@ class IndexAction extends Action
 			unset($_SESSION['username']);
 			unset($_SESSION['password']);
 		}
-		$this->display('index/login');
+		$this->index();
 	}
 }
